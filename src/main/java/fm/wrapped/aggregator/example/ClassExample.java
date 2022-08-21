aaafa157410fc49c16e0383e51d2aae4674cc170
Thomas Jaszczult <ttjazztt@gmail.com>
2022-08-20 23:46:28 -0700

    fixed object properties

--- src/main/java/fm/wrapped/aggregator/example/ClassExample.java
+++ src/main/java/fm/wrapped/aggregator/example/ClassExample.java
@@ -1,11 +1,13 @@
 package fm.wrapped.aggregator.example;
 
 public class ClassExample {
-    int someInt = 5;
-    String preFilledString = "hello";
-    String unfilledString;
+    public int someInt = 5;
+    public String preFilledString = "hello";
+    public String unfilledString;
 
     public ClassExample(String unfilled)  {
         this.unfilledString = unfilled;  // Set the initial value for the class attribute x
     }
+
+
 }
