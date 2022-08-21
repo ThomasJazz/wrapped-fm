641d3c03c5183e7931adbf358154b85a4ee6e774
Thomas Jaszczult <ttjazztt@gmail.com>
2022-08-21 15:48:31 -0700

    Update UserAuthorization.java

--- src/main/java/fm/wrapped/aggregator/payload/spotify/UserAuthorization.java
+++ src/main/java/fm/wrapped/aggregator/payload/spotify/UserAuthorization.java
@@ -5,4 +5,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class UserAuthorization {
     @JsonProperty("client_id")
     public String clientId;
+
+    @JsonProperty("response_type")
+    public String responseType="code";
+
+    @JsonProperty("redirect_uri")
+    public String redirectUri="code";
+
+    @JsonProperty("scope")
+    public String scope="code";
 }
